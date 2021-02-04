51c358431b540aabcf2b9f2bcde5cb1695b6b46b
Saif Basheer <saif191020@gmail.com>
2021-02-04 22:18:24 +0530

    Added route and views

--- website/__init__.py
+++ website/__init__.py
@@ -3,4 +3,10 @@ from flask import Flask
 def create_app():
     app =Flask(__name__)
     app.config['SECRET_KEY'] ="f98c9d9340dc4bee35b4a18d79d0fba231d0d5b94ae7613b5c14ee017461dc22"
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views,url_prefix='/')
+    app.register_blueprint(auth,url_prefix='/')
     return app
