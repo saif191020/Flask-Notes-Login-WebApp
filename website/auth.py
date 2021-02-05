5ac9e5893a23c5f72a9a4b48cf79e8aa3eb6782b
Saif Basheer <saif191020@gmail.com>
2021-02-05 11:26:18 +0530

    Added Template Html

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
+
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html")
+
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
+
 @auth.route('/sign-up')
 def signup():
-    return "<p>Sign up</p>"
+    return render_template("signup.html")
