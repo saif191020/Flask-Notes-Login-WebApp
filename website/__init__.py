68a9749da21d72d25af4888b8b0a87490a593cad
Saif Basheer <saif191020@gmail.com>
2021-02-06 16:15:52 +0530

    Flask Login Module

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -15,12 +16,21 @@ def create_app():
     from .views import views
     from .auth import auth
 
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     from .models import User, Note
 
     create_database(app)
 
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
 
 
--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
+from sqlalchemy.sql.expression import true
 from .models import User
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user, current_user, login_required
+
 auth = Blueprint('auth', __name__)
 
 
@@ -15,6 +18,7 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash("Logged in Successfully!", category="success")
+                login_user(user,remember=True)
                 return redirect(url_for('views.home'))
             else:
                 flash("Incorrect password!", category="error")
@@ -25,8 +29,10 @@ def login():
 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
@@ -56,6 +62,7 @@ def signup():
             db.session.add(new_user)
             db.session.commit()
             flash("Account Created ! ", category="success")
+            login_user(user,remember=True)
             return redirect(url_for('views.home'))
 
     return render_template("signup.html")
