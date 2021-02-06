4cb8c0a55ff3144aa30b1c684743f18450d2a927
Saif Basheer <saif191020@gmail.com>
2021-02-06 15:58:13 +0530

    Added authentication stuff

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,26 @@
-from flask import Blueprint, render_template, request, flash
-
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == "POST":
+        email = request.form['email']
+        password = request.form['password']
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash("Logged in Successfully!", category="success")
+                return redirect(url_for('views.home'))
+            else:
+                flash("Incorrect password!", category="error")
+        else:
+            flash("E-Mail does not exist!", category="error")
+
     return render_template("login.html")
 
 
@@ -20,7 +36,10 @@ def signup():
         firstName = request.form['firstName']
         password1 = request.form['password1']
         password2 = request.form['password2']
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash("Email Already exist!", category="error")
+        elif len(email) < 4:
             flash("Email should be atleast 4 characters long ", category="error")
             pass
         elif len(firstName) < 2:
@@ -32,6 +51,11 @@ def signup():
         elif len(password1) < 8:
             flash("Password Should be atleast 8 characters long ", category="error")
         else:
+            new_user = User(email=email, first_name=firstName, password=generate_password_hash(
+                password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash("Account Created ! ", category="success")
+            return redirect(url_for('views.home'))
 
     return render_template("signup.html")
