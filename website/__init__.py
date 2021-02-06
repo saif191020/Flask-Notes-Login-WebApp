8d504460f5e305ec5e3e2dc773b1c608c2aac63a
Saif Basheer <saif191020@gmail.com>
2021-02-06 14:38:01 +0530

    Corrected a typo

--- website/__init__.py
+++ website/__init__.py
@@ -9,7 +9,7 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "f98c9d9340dc4bee35b4a18d79d0fba231d0d5b94ae7613b5c14ee017461dc22"
-    app.config['SQLAlCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app=app)
 
     from .views import views
